02196072621959e09632e1d2057543d776bdf55a
mharoon1434 <m.haroon01010@gmail.com>
2026-05-03 22:53:47 +0500

    add result box

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ class passwrodgenerater:
         self.root.geometry("420x520")
         self.root.config(bg="#0b032d")
 
+        # label name
         label=tk.Label(self.root, text="Password Generator", 
-        font=("Arial", 20), fg="#fff", anchor="w", bg="#0b032d")
+        font=("Times New Roman", 20), fg="#fff", anchor="w", bg="#0b032d")
         label.pack(fill="x", padx=10, pady=10)
         
+        # result box
+        result_box=tk.Entry(self.root, width=48,justify='center',  readonlybackground="#0b032d", fg="#fff")
+        result_box.pack(ipady=8, pady=20)
+        result_box.config(state='normal')
+        result_box.insert(0, "CLICK GENERATE")
+        result_box.config(state="readonly", font=("Times New Roman", 12))
         self.root.mainloop()
 passwrodgenerater()
