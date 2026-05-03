c8808594c7955a73a95a2471511fb889a4e0d7f6
mharoon1434 <m.haroon01010@gmail.com>
2026-05-03 23:37:01 +0500

    set length

--- main.py
+++ main.py
@@ -14,9 +14,17 @@ class passwrodgenerater:
         
         # result box
         result_box=tk.Entry(self.root, width=48,justify='center',  readonlybackground="#0b032d", fg="#fff")
-        result_box.pack(ipady=8, pady=20)
+        result_box.pack(ipady=8, pady=5)
         result_box.config(state='normal')
         result_box.insert(0, "CLICK GENERATE")
         result_box.config(state="readonly", font=("Times New Roman", 12))
+
+        # set lenght of password
+        frame_length=tk.Frame(self.root)
+        frame_length.pack()
+        tk.Label(frame_length, text="length", font=("Arial", 12),bg="#0b032d" , fg="#fff")
+        count_label=tk.Label(frame_length, text="0",bg="#0b032d", fg="#fff")
+        count_label.pack(side='left')
+
         self.root.mainloop()
 passwrodgenerater()
